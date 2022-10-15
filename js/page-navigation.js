a01072ba382b96918ddcb977ddea37353aa1cdaa
mhevyk <maksimgevyk@gmail.com>
2022-10-15 19:55:50 +0300

    Styled arrows to navigate between pages

--- js/page-navigation.js
+++ js/page-navigation.js
@@ -2,11 +2,26 @@
     const pages = document.querySelectorAll(".page");
     const buttons = document.querySelectorAll(".page-open-button");
 
+    function setPageTitle(title) {
+        const titleContainer = document.querySelector(".header h1");
+        titleContainer.textContent = title;
+    }
+
+    function showBackButton(pageRole) {
+        const backButton = document.querySelector(".header .back");
+        if (pageRole !== "main") {
+            backButton.classList.add("open");
+        } else {
+            backButton.classList.remove("open");
+        }
+    }
+
     function hideAllPagesExcept(pageRole) {
+        showBackButton(pageRole);
         pages.forEach(page => {
             if(page.dataset.role === pageRole){
                 page.classList.add("open");
-                document.querySelector(".header h1").textContent = page.dataset.title;
+                setPageTitle(page.dataset.title);
             } else {
                 page.classList.remove("open");
             }
